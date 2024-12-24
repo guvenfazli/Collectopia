ec2f479eefa73951ce051b0866f5ffbd5866ec70
Güven Fazlı <guven.fazli@gmail.com>
2024-12-24 09:56:26 +0300

    Packages installed, Font added to Tailwind CSS

--- collectopia/app/page.tsx
+++ collectopia/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <p>Welcome.</p>
+      <p className="text-5xl font-general">Welcome.</p>
     </div>
   );
 }
--- collectopia/tailwind.config.ts
+++ collectopia/tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        general: ["Lato", "sans-serif"]
+      }
     },
   },
   plugins: [],
