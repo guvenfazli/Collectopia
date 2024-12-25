3e063f85d1eddc696ae7b2a905275e05324f56a2
Güven Fazlı <guven.fazli@gmail.com>
2024-12-25 04:29:15 +0300

    Account creation, password hashing is completed.

--- collectopia/backend/controllers/authController.js
+++ collectopia/backend/controllers/authController.js
@@ -1,23 +1,44 @@
+// MODELS
+
+const User = require('../models/userModel')
+
+// UTILS
 const { validationResult } = require('express-validator')
 const { throwError } = require('../utils/throwError')
+const bcrypt = require('bcryptjs')
 
-exports.register = (req, res, next) => {
+exports.register = async (req, res, next) => {
   const { name, surname, email, password, interests } = req.body
   const parsedInterests = JSON.parse(interests)
   const errors = validationResult(req)
 
   try {
-    if (name.length <= 5) {
+
+    if (!errors.isEmpty()) {
       throwError(errors.array()[0].msg, 410)
     } else if (parsedInterests.length === 0) {
       throwError('Please choose at least one topic', 410)
     }
 
+    const alreadyExists = await User.findOne({ email: email })
+    if (alreadyExists) {
+      throwError('This account already exists!', 410)
+    }
+
+    const hashedPw = await bcrypt.hash(password, 12)
+
+    const createdUser = new User({
+      name,
+      surname,
+      email,
+      password: hashedPw,
+      interests: parsedInterests
+    })
+
+    await createdUser.save()
+    res.status(201).json({ message: 'Account created successfully! Welcome!' })
   } catch (err) {
     next(err)
   }
-
-
-  // Array must be Parsed
 
 }
