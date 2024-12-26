ca7fd2f1bb994d35a837fa2c6329db7522a3f781
Güven Fazlı <guven.fazli@gmail.com>
2024-12-26 03:54:14 +0300

    image preview is done, buttons left for navigation

--- collectopia/components/addItem/chooseFileAndSubmit.tsx
+++ collectopia/components/addItem/chooseFileAndSubmit.tsx
@@ -9,11 +9,22 @@ export default function ChooseFileAndSubmit({ setImagePicker, setImageShowcase }
 
 
   function getImage(e: BaseSyntheticEvent) {
-    console.log(e.target.value)
-    const file = e.target.files[0]
-    const fileLink = URL.createObjectURL(file)
-    console.log(fileLink)
-    setImageShowcase(fileLink)
+    for (let i = 0; i < e.target.files.length; i++) {
+      const file = e.target.files[i]
+      console.log(file)
+      const fileLink = URL.createObjectURL(file)
+      setImageShowcase((prev: string[]) => {
+        const updated = [...prev]
+        updated.push(fileLink)
+        return updated
+      })
+    }
+
+
+    /*     console.log(file)
+        const fileLink = URL.createObjectURL(file)
+        console.log(fileLink) */
+    /*   setImageShowcase(fileLink) */
   }
 
 
@@ -21,9 +32,6 @@ export default function ChooseFileAndSubmit({ setImagePicker, setImageShowcase }
   return (
     <div className="flex flex-row items-start justify-center gap-5">
       <input type="file" multiple onChange={(e) => getImage(e)} required className="file:font-medium file:duration-150 file:bg-blue-800 file:py-1 file:px-5 file:border-none file:text-white file:rounded-md file:shadow-2xl file:cursor-pointer file:hover:bg-blue-950" />
-
-
-      {imagePrev && <Image src={imagePrev} fill alt="test" className="w-72 h-72"></Image>}
 
       <button className="duration-150 py-1 px-5 text-white rounded-md font-medium bg-blue-800 hover:bg-blue-950">Submit</button>
     </div>
--- collectopia/components/addItem/imageShowcase.tsx
+++ collectopia/components/addItem/imageShowcase.tsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
 import Image from "next/image";
+
 
 export default function ImageShowcase({ imageShowcase }) {
 
+  const [imageNavigator, setImageNavigator] = useState<number>(0)
 
-
-
+  console.log(imageShowcase[0])
   return (
-    <div className="flex w-full items-center justify-center relative h-96 border border-black">
-      {!imageShowcase ? <p>You did not choose any image yet!</p> : <Image fill alt="uploadedImage" src={imageShowcase}></Image>}
+    <div>
+      <div className="flex items-center justify-center overflow-hidden relative h-96 border border-black">
+        {!imageShowcase ? <p>You did not choose any image yet!</p> : imageShowcase.map((img) =>
+          <Image
+            key={img}
+            width={0} height={0}
+            alt="uploadedImage"
+            style={{ height: '100%', width: '100%', translate: `${imageNavigator * -100}%`, zIndex: 0 }}
+            src={img}>
+          </Image>
+        )}
+      </div>
+      <button onClick={() => setImageNavigator(prev => prev -= 1)} className="z-50 text-3xl text-white">Prev</button>
+      <button onClick={() => setImageNavigator(prev => prev += 1)} className="z-50 text-white">Next</button>
     </div>
   )
 }
