135a07fe48ed3eb0aad9ffd65d7ed24ef589d455
Güven Fazlı <guven.fazli@gmail.com>
2024-12-27 07:07:03 +0300

    New color added to loader component

--- collectopia/app/page.tsx
+++ collectopia/app/page.tsx
@@ -1,13 +1,9 @@
 import Link from "next/link";
 export default function Home() {
-
-
-
   return (
     <div>
       <p className="text-5xl font-general">Welcome.</p>
       <Link href={'/userAuth?mode=login'}>Auth Page</Link>
-   
     </div>
   );
 }
