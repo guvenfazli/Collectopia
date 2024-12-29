f4bce6bab1fbd900f657e7f3a4c9b63a8f536eba
Güven Fazlı <guven.fazli@gmail.com>
2024-12-30 01:03:14 +0300

    UserLiveAuction card created, styled, section created

--- collectopia/components/profile/liveAuctions/userAuctionCard.tsx
+++ collectopia/components/profile/liveAuctions/userAuctionCard.tsx
@@ -1,3 +1,46 @@
-export default function UserAuctionCard(){
-  
+import CardInformation from "../cardInformation"
+import dayjs from "dayjs"
+
+type FetchedAuction = {
+  _id: string,
+  minValue: number,
+  buyout: number,
+  deadline: number,
+  createdAt: string,
+  followers: string[],
+  item: any
+}
+
+type ComponentsProps = {
+  auction: FetchedAuction;
+  isListing: boolean
+}
+
+
+
+
+
+export default function UserAuctionCard({ auction, isListing }: ComponentsProps) {
+
+  const dateDataConverted = dayjs(auction.deadline) // Formats the date
+
+
+
+
+  return (
+    <div className={`flex bg-blue-200 text-nowrap overflow-hidden duration-700 ease-in-out flex-shrink-0 border border-blue-300 h-full flex-col items-start justify-start shadow-slate-800 shadow-xl rounded-lg ${!isListing ? '-mr-20 w-full' : 'mr-0 w-1/4'}`}>
+
+
+
+
+
+      <div className={`flex flex-col w-full p-1 gap-3 justify-start items-start ${!isListing ? 'h-0' : 'h-auto'}`}>
+        <CardInformation fetchedItemInfo={auction.item.title} title="Title" />
+        <CardInformation fetchedItemInfo={auction.minValue + ' $'} title="Minimum Auction Value" />
+        <CardInformation fetchedItemInfo={auction.buyout + ' $'} title="Buyout Value" />
+        <CardInformation tagList={auction.item.tagList} title="Tags" />
+        <CardInformation fetchedItemInfo={dateDataConverted.format("DD/MM/YYYY")} title="Deadline" />
+      </div>
+    </div>
+  )
 }
--- collectopia/components/profile/liveAuctions/userLiveAuctions.tsx
+++ collectopia/components/profile/liveAuctions/userLiveAuctions.tsx
@@ -1,10 +1,35 @@
 "use client"
 
-export default function UserLiveAuctions({ userAuctions }) {
+import { useState } from "react"
+import UserAuctionCard from "./userAuctionCard"
+
+
+type FetchedAuction = {
+  _id: string,
+  minValue: number,
+  buyout: number,
+  deadline: number,
+  createdAt: string,
+  followers: string[],
+  item: any
+}
+
+
+type ComponentsProp = {
+  userAuctions: FetchedAuction[]
+}
+
+
+
+
+export default function UserLiveAuctions({ userAuctions }: ComponentsProp) {
+
+  const [isListing, setIsListing] = useState<boolean>(false)
+  const [listingNavigator, setListingNavigator] = useState<number>(0)
 
 
   return (
-    <div className="flex flex-col w-full items-start justify-start gap-5">
+    <div onMouseLeave={() => setIsListing(false)} className="flex flex-col w-full items-start justify-start gap-5">
 
       <div className="flex flex-col w-full items-center justify-start">
         <p className="text-orange-600 text-3xl font-logo tracking-wide">Active Listings <span className="text-base">({userAuctions.length})</span></p>
@@ -12,6 +37,9 @@ export default function UserLiveAuctions({ userAuctions }) {
       </div>
 
       <div className="flex flex-row w-full  relative overflow-hidden">
+        <div onMouseEnter={() => setIsListing(true)} style={{ translate: `${listingNavigator * -50}%` }} className={`flex flex-row h-auto items-center justify-start ${!isListing ? 'w-44' : 'gap-5 w-full'} duration-1000`}>
+          {userAuctions.map((auction: FetchedAuction) => <UserAuctionCard key={auction._id} auction={auction} isListing={isListing} />)}
+        </div>
 
       </div>
     </div>
