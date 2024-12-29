3a2c6e5738ec96a898da7a88cad7b9c388a56e95
Güven Fazlı <guven.fazli@gmail.com>
2024-12-29 05:21:12 +0300

    ShadCN carousel component added

--- collectopia/components/addItem/imageShowcase.tsx
+++ collectopia/components/addItem/imageShowcase.tsx
@@ -1,6 +1,14 @@
 import { useState, useEffect } from "react";
 import Image from "next/image";
 
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+
 type componentPropType = {
   imageShowcase: string[]
 }
@@ -15,24 +23,27 @@ export default function ImageShowcase({ imageShowcase }: componentPropType) {
 
   return (
     <div>
-      <div className="flex w-full items-center mb-4 overflow-hidden relative h-96">
+      <div className="flex w-full border items-center mb-4 overflow-hidden relative h-96">
         {imageShowcase.length === 0 && <div className="flex w-full justify-center items-center"><p>You did not choose any image yet!</p></div>}
-        {imageShowcase.length >= 0 && imageShowcase.map((img) =>
-          <div key={img} style={{ translate: `${imageNavigator * -100}%` }} className="flex w-full h-full overflow-hidden flex-shrink-0 duration-700">
-            <Image
-              width={0} height={0}
-              alt="uploadedImage"
-              style={{ height: '100%', width: '100%', zIndex: 0, transitionDuration: "700ms" }}
-              src={img}>
-            </Image>
-          </div>
-        )}
+        <Carousel className="w-full">
+          <CarouselContent className="h-96 border border-red-700">
+            {imageShowcase.map((img) =>
+              <CarouselItem className="relative" key={img}>
+                <Image
+                  fill
+                  alt="uploadedImage"
+                  src={img.replaceAll(/\\/g, "/")}
+                  style={{ objectFit: "contain" }} />
+              </CarouselItem>
+            )}
+          </CarouselContent>
+        </Carousel>
       </div>
 
-      <div className="flex flex-row justify-between items-center">
+{/*       <div className="flex flex-row justify-between items-center">
         <button onClick={() => setImageNavigator(prev => prev -= 1)} disabled={imageNavigator === 0 || imageShowcase.length === 0} className="bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-blue-700 hover:shadow-lg active:scale-95 transition-transform duration-150 ease-in-out disabled:bg-blue-300">&lt;</button>
         <button onClick={() => setImageNavigator(prev => prev += 1)} disabled={imageNavigator === imageShowcase.length - 1 || imageShowcase.length === 0} className="bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-blue-700 hover:shadow-lg active:scale-95 transition-transform duration-150 ease-in-out disabled:bg-blue-300">&gt;</button>
-      </div>
+      </div> */}
 
     </div>
   )
