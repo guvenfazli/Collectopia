7543f1d7b4398cdeb84fc5dba6b0067af58e8b62
Güven Fazlı <guven.fazli@gmail.com>
2024-12-30 01:12:04 +0300

    Date formating bug fixed

--- collectopia/components/profile/liveAuctions/userAuctionCard.tsx
+++ collectopia/components/profile/liveAuctions/userAuctionCard.tsx
@@ -1,3 +1,9 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel"
+
 import CardInformation from "../cardInformation"
 import dayjs from "dayjs"
 
@@ -17,12 +23,12 @@ type ComponentsProps = {
 }
 
 
-
+import Image from "next/image"
 
 
 export default function UserAuctionCard({ auction, isListing }: ComponentsProps) {
 
-  const dateDataConverted = dayjs(auction.deadline) // Formats the date
+  const dateDataConverted = dayjs.unix(auction.deadline) // Formats the date
 
 
 
@@ -30,16 +36,31 @@ export default function UserAuctionCard({ auction, isListing }: ComponentsProps)
   return (
     <div className={`flex bg-blue-200 text-nowrap overflow-hidden duration-700 ease-in-out flex-shrink-0 border border-blue-300 h-full flex-col items-start justify-start shadow-slate-800 shadow-xl rounded-lg ${!isListing ? '-mr-20 w-full' : 'mr-0 w-1/4'}`}>
 
+      <div className="flex w-full items-start mb-4 min-h-44 overflow-hidden relative">
+        <Carousel className="w-full">
+          <CarouselContent className="h-44">
+            {auction.item.imageList.map((img: string) =>
+              <CarouselItem className="relative" key={img}>
+                <div className="absolute blur-sm top-0 right-0 bottom-0 left-0" style={{ background: `url(http://localhost:8080/${img.replaceAll(/\\/g, "/")})`, backgroundPosition: "center", backgroundSize: "contain" }} />
+                <Image
+                  fill
+                  alt="uploadedImage"
+                  src={`http://localhost:8080/${img.replaceAll(/\\/g, "/")}`}
+                  style={{ objectFit: "contain" }} />
+              </CarouselItem>
+            )}
+          </CarouselContent>
+        </Carousel>
+      </div>
 
 
 
-
-      <div className={`flex flex-col w-full p-1 gap-3 justify-start items-start ${!isListing ? 'h-0' : 'h-auto'}`}>
+      <div className={`flex flex-col w-full p-1 gap-3 justify-start items-start duration-150 ${!isListing ? 'h-0' : 'h-auto'}`}>
         <CardInformation fetchedItemInfo={auction.item.title} title="Title" />
         <CardInformation fetchedItemInfo={auction.minValue + ' $'} title="Minimum Auction Value" />
         <CardInformation fetchedItemInfo={auction.buyout + ' $'} title="Buyout Value" />
         <CardInformation tagList={auction.item.tagList} title="Tags" />
-        <CardInformation fetchedItemInfo={dateDataConverted.format("DD/MM/YYYY")} title="Deadline" />
+        <CardInformation fetchedItemInfo={dayjs(dateDataConverted).format("DD/MM/YYYY")} title="Deadline" />
       </div>
     </div>
   )
