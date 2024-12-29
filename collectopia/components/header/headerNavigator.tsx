a3541d6c04b9832a65d3e5cf328d63f18704422e
Güven Fazlı <guven.fazli@gmail.com>
2024-12-29 07:23:44 +0300

    Tooltips added to Header Navigator

--- collectopia/components/header/headerNavigator.tsx
+++ collectopia/components/header/headerNavigator.tsx
@@ -24,21 +24,81 @@ import ItemsForAuctionCreation from "./itemsForAuctionCreation";
 export default function HeaderNavigator() {
   return (
     <nav className="flex flex-row justify-around items-center gap-5 text-lg">
-      <Link className="shadow-sm shadow-orange-800 hover:shadow-md hover:shadow-orange-900 transition-all duration-200 border border-red-900 p-2 py-2 bg-white rounded-full hover:bg-orange-300" href={'/'}><FaHome /></Link>
+
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link className="shadow-sm shadow-orange-800 hover:shadow-md hover:shadow-orange-900 transition-all duration-200 border border-red-900 p-2 py-2 bg-white rounded-full hover:bg-orange-300" href={'/'}><FaHome /></Link>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Home</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+
 
       <Dialog>
-        <DialogTrigger className="shadow-sm shadow-orange-800 hover:shadow-md hover:shadow-orange-900 transition-all duration-200 border border-red-900 p-2 py-2 bg-white rounded-full hover:bg-orange-300"><IoMdMegaphone /></DialogTrigger>
+        <DialogTrigger className="shadow-sm shadow-orange-800 hover:shadow-md hover:shadow-orange-900 transition-all duration-200 border border-red-900 p-2 py-2 bg-white rounded-full hover:bg-orange-300">
+
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <IoMdMegaphone />
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Create a Listing</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+
+        </DialogTrigger>
+
         <DialogContent className="bg-orange-50 text-lg text-orange-800 flex flex-col border border-orange-800">
           <DialogHeader>
             <DialogTitle className="font-logo tracking-widest text-xl">Create a listing <span className="text-sm font-general tracking-wide">(Click the name of the item that you want to list.)</span></DialogTitle>
           </DialogHeader>
           <ItemsForAuctionCreation />
         </DialogContent>
+
       </Dialog>
 
-      <Link className="shadow-sm shadow-orange-800 hover:shadow-md hover:shadow-orange-900 transition-all duration-200 border border-red-900 p-2 py-2 bg-white rounded-full hover:bg-orange-300" href={'/addItem'}><IoMdAddCircle /></Link>
-      <Link className="shadow-sm shadow-orange-800 hover:shadow-md hover:shadow-orange-900 transition-all duration-200 border border-red-900 p-2 py-2 bg-white rounded-full hover:bg-orange-300" href={'/myTrackings'}><GiPayMoney /></Link>
-      <Link className="shadow-sm shadow-orange-800 hover:shadow-md hover:shadow-orange-900 transition-all duration-200 border border-red-900 p-2 py-2 bg-white rounded-full hover:bg-orange-300" href={'/myAuctions'}><GiReceiveMoney /></Link>
+
+
+
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link className="shadow-sm shadow-orange-800 hover:shadow-md hover:shadow-orange-900 transition-all duration-200 border border-red-900 p-2 py-2 bg-white rounded-full hover:bg-orange-300" href={'/addItem'}><IoMdAddCircle /></Link>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Add an Item</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link className="shadow-sm shadow-orange-800 hover:shadow-md hover:shadow-orange-900 transition-all duration-200 border border-red-900 p-2 py-2 bg-white rounded-full hover:bg-orange-300" href={'/myTrackings'}><GiPayMoney /></Link>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>My Trackings</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link className="shadow-sm shadow-orange-800 hover:shadow-md hover:shadow-orange-900 transition-all duration-200 border border-red-900 p-2 py-2 bg-white rounded-full hover:bg-orange-300" href={'/myAuctions'}><GiReceiveMoney /></Link>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>My Auctions</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+
+
     </nav>
   )
 }
--- collectopia/components/profile/inventory.tsx
+++ collectopia/components/profile/inventory.tsx
@@ -1,5 +1,7 @@
 "use client"
 import { useState, useEffect } from "react"
+import { useSelector } from "react-redux"
+import { useParams } from "next/navigation"
 import InventoryItemCard from "./inventoryItemCard"
 
 type FetchedItemType = {
@@ -21,6 +23,10 @@ type ComponentsProp = {
 }
 
 export default function UsersInventory({ userInventory }: ComponentsProp) {
+
+  const loggedUser = useSelector((state: { auth: { userInfo: { userInfo: any } } }) => state.auth.userInfo.userInfo)
+
+  const { userId } = useParams()
 
   const [isInventory, setIsInventory] = useState<boolean>(false)
   const [inventoryNavigator, setInventoryNavigator] = useState(0)
@@ -55,8 +61,15 @@ export default function UsersInventory({ userInventory }: ComponentsProp) {
 
   return (
     <div onMouseLeave={() => setIsInventory(false)} className="flex flex-col w-full items-start justify-start gap-5">
+
+
+
+
       <div className="flex flex-col w-full items-center justify-start">
         <p className="text-orange-600 text-3xl font-logo tracking-wide">Inventory  <span className="text-base">({userInventory.length})</span></p>
+        {(userInventory.length === 0 && userId === loggedUser.id) ? <p>You have no items!</p> : <p>This user has no items!</p>}
+
+
         {userInventory.length > 0 &&
           <div className="flex flex-row w-full justify-between items-center">
             <button disabled={inventoryNavigator === 0 || !isInventory} onClick={() => setInventoryNavigator(prev => prev -= 1)} className="py-1 px-2 duration-150 bg-orange-600 text-white shadow-md rounded-lg hover:bg-orange-700 hover:shadow-lg disabled:bg-orange-300 active:scale-95">Previous</button>
@@ -67,13 +80,9 @@ export default function UsersInventory({ userInventory }: ComponentsProp) {
 
 
       <div className="flex flex-row w-full  relative overflow-hidden">
-        {/*         <div onMouseEnter={() => setSliderNavigator(prev => !prev)} onMouseLeave={() => setSliderNavigator(prev => !prev)} className="top-0 bottom-0 border border-black left-0 w-24 absolute z-40" /> */}
         <div onMouseEnter={() => setIsInventory(true)} style={{ translate: `${inventoryNavigator * -50}%` }} className={`flex flex-row h-auto items-center justify-start ${!isInventory ? 'w-44' : 'gap-5 w-full'} duration-1000`}>
           {userInventory.map((item: FetchedItemType) => <InventoryItemCard key={item._id} fetchedItem={item} isInventory={isInventory} />)}
         </div>
-        {/*         <div onMouseEnter={() => setSliderNavigator(prev => !prev)} onMouseLeave={() => setSliderNavigator(prev => !prev)} className="top-0 bottom-0 right-0 w-24 absolute z-40" /> */}
-
-
       </div>
     </div>
   )
