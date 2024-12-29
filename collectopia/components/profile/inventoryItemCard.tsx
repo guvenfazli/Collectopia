0ccd6a0f3b9f1c2ad7ce1ac3a8f22c041b0ffc6c
Güven Fazlı <guven.fazli@gmail.com>
2024-12-29 05:36:00 +0300

    Profile Inventory Item Card image carousel replaced with ShadCN (Better performance))

--- collectopia/components/profile/inventoryItemCard.tsx
+++ collectopia/components/profile/inventoryItemCard.tsx
@@ -1,3 +1,11 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+
 import { useState } from "react"
 import { HiArrowSmallRight } from "react-icons/hi2";
 import { HiArrowSmallLeft } from "react-icons/hi2";
@@ -32,23 +40,22 @@ export default function InventoryItemCard({ fetchedItem, isInventory }: Componen
   return (
     <div className={`flex bg-orange-200 text-nowrap overflow-hidden duration-700 ease-in-out flex-shrink-0 border border-orange-300 h-full flex-col items-start justify-start shadow-slate-800 shadow-xl ${!isInventory ? '-mr-20 w-full' : 'mr-0 w-1/3'}`}>
       <div className="flex w-full items-start mb-4 min-h-72 pt-4 overflow-hidden relative">
-        {fetchedItem.imageList.map((img: string) =>
-          <div key={img} style={{ translate: `${imageNavigator * -100}%` }} className="flex w-full h-full overflow-hidden flex-shrink-0 duration-700">
-            {<Image
-              fill
-            
-              alt="uploadedImage"
-              style={{ zIndex: 0, transitionDuration: "700ms"}}
-              src={`http://localhost:8080/${img.replaceAll(/\\/g, "/")}`}>
-            </Image>}
-          </div>
-        )}
+        <Carousel className="w-full">
+          <CarouselContent className="h-96">
+            {fetchedItem.imageList.map((img) =>
+              <CarouselItem className="relative" key={img}>
+                <div className="absolute blur-sm top-0 right-0 bottom-0 left-0" style={{ background: `url(${img.replaceAll(/\\/g, "/")})`, backgroundPosition: "center", backgroundSize: "contain" }} />
+                <Image
+                  fill
+                  alt="uploadedImage"
+                  src={`http://localhost:8080/${img.replaceAll(/\\/g, "/")}`}
+                  style={{ objectFit: "cover" }} />
+              </CarouselItem>
+            )}
+          </CarouselContent>
+        </Carousel>
       </div>
 
-      <div className="flex flex-row w-full p-1 justify-between items-center">
-        <button onClick={() => setImageNavigator(prev => prev -= 1)} disabled={imageNavigator === 0 || fetchedItem.imageList.length === 0} className="bg-orange-600 text-white font-semibold py-1 px-1 rounded-lg shadow-md hover:bg-orange-700 hover:shadow-lg active:scale-95 transition-transform duration-150 ease-in-out disabled:bg-orange-300"><HiArrowSmallLeft /></button>
-        <button onClick={() => setImageNavigator(prev => prev += 1)} disabled={imageNavigator === fetchedItem.imageList.length - 1 || fetchedItem.imageList.length === 0} className="bg-orange-600 text-white font-semibold py-1 px-1 rounded-lg shadow-md hover:bg-orange-700 hover:shadow-lg active:scale-95 transition-transform duration-150 ease-in-out disabled:bg-orange-300"><HiArrowSmallRight /></button>
-      </div>
 
       <div className="flex flex-col w-full p-1 gap-3 justify-start items-start">
         <CardInformation fetchedItemInfo={fetchedItem.title} title="Title" />
