3fac0cb5b2ae2a75d059967beba7ff2a43b43faa
Güven Fazlı <guven.fazli@gmail.com>
2024-12-31 23:24:31 +0300

    Homepage last 24 hrs section created

--- collectopia/app/page.tsx
+++ collectopia/app/page.tsx
@@ -1,7 +1,14 @@
 import Link from "next/link";
+import LastAuctions from "@/components/homepage/lastAuctions/lastAuctions";
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col justify-start items-center w-full">
+      
+      <div className="flex flex-col p-3 w-9/12 bg-white">
+        <LastAuctions />
+      </div>
+
+
       <p className="text-5xl font-general">Welcome.</p>
       <Link href={'/userAuth?mode=login'}>Auth Page</Link>
       <Link href={'/profile/676cd2e40bddea53d1a9ca03'}>Test</Link>
---
+++ collectopia/components/homepage/lastAuctions/lastAuctions.tsx
@@ -0,0 +1,21 @@
+export default function LastAuctions() {
+  return (
+    <div className="flex flex-col">
+      <div className="flex flex-row w-full justify-start items-center">
+        <p className="text-4xl text-orange-800 font-logo tracking-widest italic">Auctions in the Last 24 Hour</p>
+      </div>
+
+      <div className="flex flex-row gap-2">
+
+      </div>
+
+      <div className="flex flex-row border items-start p-3 w-full">
+        <p>ItemCard</p>
+        <p>ItemCard</p>
+        <p>ItemCard</p>
+        <p>ItemCard</p>
+
+      </div>
+    </div>
+  )
+}
