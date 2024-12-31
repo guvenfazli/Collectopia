7153f9c22ff5bdff88c6f17cce40c06a0aa11071
Güven Fazlı <guven.fazli@gmail.com>
2024-12-31 03:36:15 +0300

    Follow button added, tooltip added

--- collectopia/components/profile/mainInformation.tsx
+++ collectopia/components/profile/mainInformation.tsx
@@ -1,4 +1,12 @@
 "use client"
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+
 import { RiUserFollowLine } from "react-icons/ri";
 import dayjs from "dayjs";
 
@@ -28,6 +36,17 @@ export default function MainInformation({ userInformation }: ComponentsProp) {
       <div className="flex flex-row justify-start items-end gap-5">
         <p className="text-5xl font-medium font-logo text-slate-800 tracking-wider">{userInformation.name + " " + userInformation.surname}</p>
         <p className="text-xl font-medium font-logo text-slate-800"><span className="text-sm font-sans text-slate-600 mr-1">Joined:</span> {dateDataConverted.format("DD/MM/YYYY")}</p>
+        <TooltipProvider delayDuration={300}>
+          <Tooltip>
+            <TooltipTrigger className="p-1 border bg-orange-600 rounded-3xl text-white hover:bg-orange-400 duration-150">
+              <RiUserFollowLine />
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Follow User</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+
       </div>
 
       <div className="flex flex-row justify-start items-end gap-5">
