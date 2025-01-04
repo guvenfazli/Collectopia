b9aa5dbb9bb13190c025ae5c21240d6d14c4ad03
Güven Fazlı <guven.fazli@gmail.com>
2025-01-04 06:15:08 +0300

    session fetching after refresh bug is fixed

--- collectopia/app/profile/[userId]/page.tsx
+++ collectopia/app/profile/[userId]/page.tsx
@@ -1,44 +1,53 @@
+"use client"
+import { useState, useEffect } from "react"
 import MainInformation from "@/components/profile/mainInformation"
 import UsersInventory from "@/components/profile/inventory"
 import UserLiveAuctions from "@/components/profile/liveAuctions/userLiveAuctions"
-import { cookies } from "next/headers"
-export default async function UserProfilePage({ params }: any) {
+import { useParams } from "next/navigation"
 
-  const { userId } = await params
-  const cookie = await cookies()
+export default function UserProfilePage() {
 
-  try {
-    const response = await fetch(`http://localhost:8080/findUser/${userId}`, {
-      headers: { Cookie: cookie.toString() }
-    })
+  const { userId } = useParams()
+  const [foundUser, setfoundUser] = useState<any>()
 
-    if (!response.ok) {
+  useEffect(() => {
+    async function fetchUser() {
+      const response = await fetch(`http://localhost:8080/findUser/${userId}`, {
+        credentials: "include"
+      })
+
+      if (!response.ok) {
+        const resData = await response.json()
+        const error = new Error(resData.message)
+        throw error
+      }
+
       const resData = await response.json()
-      const error = new Error(resData.message)
-      throw error
+
+      setfoundUser(resData.foundUser)
     }
 
-    const resData = await response.json()
-    
+    fetchUser()
+  }, [])
 
+  if (foundUser) {
     return (
       <div className="flex flex-col h-auto justify-start items-center ">
         <div className="flex bg-white border-b-orange-300 border-b w-2/3 p-5 shadow-sm shadow-slate-800">
-          <MainInformation userInformation={resData.foundUser} />
+          <MainInformation userInformation={foundUser} />
         </div>
 
         <div className="flex bg-white border-b-orange-800 w-2/3 p-5 shadow-sm shadow-slate-800">
-          <UsersInventory userInventory={resData.foundUser.items} />
+          <UsersInventory userInventory={foundUser.items} />
         </div>
 
         <div className="flex bg-white border-b-orange-800 w-2/3 p-5 shadow-sm shadow-slate-800">
-          <UserLiveAuctions userAuctions={resData.foundUser.auctions} />
+          <UserLiveAuctions userAuctions={foundUser.auctions} />
         </div>
       </div>
     )
-
-  } catch (err: any) {
-    console.log(err.message)
   }
+
+
 
 }
