667af885709c1dfdd5b8026ce580d874f4b8d01a
Güven Fazlı <guven.fazli@gmail.com>
2025-01-07 09:09:18 +0300

    break-words and space did not fix, will change the location of the divs.

--- collectopia/components/userInbox/recievedMessage.tsx
+++ collectopia/components/userInbox/recievedMessage.tsx
@@ -22,12 +22,12 @@ export default function RecievedMessage({ message }: ComponentProps) {
         <p>{message.sender.name + ' ' + message.sender.surname}</p>
       </div>
 
-      <div className="flex w-full justify-between items-start text-nowrap ">
-        <div className="flex w-full text-ellipsis">
-          <p className="text-orange-800 tracking-wide line-clamp-1">{message.message}</p>
+      <div className="flex w-full justify-between items-start text-nowrap">
+        <div className="flex space-x-4 text-ellipsis">
+          <p className="text-orange-800 tracking-wide line-clamp-1 break-words overflow-hidden">{message.message}</p>
         </div>
 
-        <div className="flex border border-red-600">
+        <div className="flex">
           <p className="text-sm italic">{dayjs(message.createdAt).format("DD/MM/YY") + ' ' + dayjs(message.createdAt).format("HH:mm")}</p>
         </div>
       </div>
