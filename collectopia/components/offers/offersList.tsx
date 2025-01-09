12201689d71dff2661a4f6413dde9abc020925bc
Güven Fazlı <guven.fazli@gmail.com>
2025-01-09 09:21:59 +0300

    Rendering the offer prototype is done

--- collectopia/components/offers/offersList.tsx
+++ collectopia/components/offers/offersList.tsx
@@ -6,13 +6,17 @@ type ComponentProps = {
 }
 
 export default function OffersList({ offersList, renderOffers }: ComponentProps) {
-  
+
 
 
 
   return (
     <div className="flex flex-col w-1/2 gap-2 border-r border-orange-800">
-      <ReceivedOfferCard />
+      {
+        offersList[renderOffers].map((offer: any) =>
+          <ReceivedOfferCard key={offer._id} offer={offer.offer} />
+        )
+      }
     </div>
   )
 }
--- collectopia/components/offers/receivedOfferCard.tsx
+++ collectopia/components/offers/receivedOfferCard.tsx
@@ -1,7 +1,21 @@
-export default function ReceivedOfferCard(){
+type ComponentProps = {
+  offer: any
+}
+export default function ReceivedOfferCard({ offer }: ComponentProps) {
+
+  console.log(offer)
+
   return (
-    <div>
-      <p>This will be the received offer card.</p>
+    <div className="flex flex-col gap-2 w-full">
+      <div>
+        {offer.offeredItems.map((offeredItem: any) => <p key={offeredItem._id}>{offeredItem.title}</p>)}
+      </div>
+
+
+      <div>
+        {offer.wantedItems.map((wantedItem: any) => <p key={wantedItem._id}>{wantedItem.title}</p>)}
+
+      </div>
     </div>
   )
 }
