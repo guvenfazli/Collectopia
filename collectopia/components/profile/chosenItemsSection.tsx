5fef3338c4ccd664094e6a6dbb0cbf32c3bcf735
Güven Fazlı <guven.fazli@gmail.com>
2025-01-09 05:48:32 +0300

    Styling added, conditional render messages added, state updating done, removing done, conditional adding is done

--- collectopia/components/profile/chosenItemsSection.tsx
+++ collectopia/components/profile/chosenItemsSection.tsx
@@ -11,7 +11,7 @@ type ComponentProps = {
 export default function ChosenItemsSection({ chosenItems, setChosenItems, items }: ComponentProps) {
   return (
     <div style={{ scrollbarWidth: 'thin', scrollbarColor: "#9A3412 transparent", WebkitOverflowScrolling: "touch" }} className="flex w-full justify-start items-start overflow-scroll overflow-y-hidden  gap-2 flex-nowrap">
-      {chosenItems.length === 0 ? <p>You did not choose any item.</p> : chosenItems.map((item: any) => {
+      {chosenItems.length === 0 ? <p>No Item Chosen.</p> : chosenItems.map((item: any) => {
         return (
           <ChosenOfferCard key={item._id} item={item} setChosenItems={setChosenItems} items={items} />
         )
--- collectopia/components/profile/makeAnOffer.tsx
+++ collectopia/components/profile/makeAnOffer.tsx
@@ -2,6 +2,11 @@ import { useEffect, useState } from "react";
 import ProfileOwnerItems from "./profileOwnerItems"
 import ChosenItemsSection from "./chosenItemsSection";
 
+type chosenItems = {
+  [itemOwner: string]: { userItems: any, myItems: any }[],
+}
+
+
 type ComponentProps = {
   userId: string;
   userItems: any
@@ -10,7 +15,7 @@ type ComponentProps = {
 export default function MakeAnOffer({ userId, userItems }: ComponentProps) {
 
   const [myItems, setMyItems] = useState([])
-  const [chosenItems, setChosenItems] = useState<{ userItems: string[], myItems: string[] }>({
+  const [chosenItems, setChosenItems] = useState<{ userItems: any[], myItems: any[] }>({
     userItems: [],
     myItems: []
   })
@@ -43,31 +48,37 @@ export default function MakeAnOffer({ userId, userItems }: ComponentProps) {
   }, [])
 
   return (
-    <div className="flex flex-row justify-start items-start">
-      <div className="flex flex-col gap-3 w-full border-r border-r-orange-800 h-[500px]">
-        <div className="flex flex-col gap-3 h-1/2">
-          <p className="font-logo text-sm border-b border-b-orange-800 w-full">Users Inventory</p>
-          <ProfileOwnerItems userItems={userItems} setChosenItems={setChosenItems} items={"userItems"} />
+    <div className="flex flex-col justify-start items-start">
+      <div className="flex flex-row w-full justify-start items-start mb-4 border-b border-b-orange-800">
+        <div className="flex flex-col gap-3 w-full border-r border-r-orange-800 h-[500px]">
+          <div className="flex flex-col gap-3 h-1/2">
+            <p className="font-logo text-sm border-b border-b-orange-800 w-full">Users Inventory</p>
+            <ProfileOwnerItems userItems={userItems} setChosenItems={setChosenItems} items={"userItems"} />
+          </div>
+
+          <div className="flex flex-col gap-3 w-full h-1/2">
+            <p className="font-logo text-sm border-b border-b-orange-800 ">Your Inventory</p>
+            <ProfileOwnerItems userItems={myItems} setChosenItems={setChosenItems} items={"myItems"} />
+          </div>
         </div>
 
-        <div className="flex flex-col gap-3 w-full h-1/2">
-          <p className="font-logo text-sm border-b border-b-orange-800 ">Your Inventory</p>
-          <ProfileOwnerItems userItems={myItems} setChosenItems={setChosenItems} items={"myItems"} />
+        {/* OFFER SECTION */}
+
+        <div className="flex flex-col gap-3 w-full h-[500px]">
+          <div className="flex flex-col gap-3 w-full h-1/2">
+            <p className="font-logo text-sm border-b border-b-orange-800 w-full">You Want</p>
+            <ChosenItemsSection chosenItems={chosenItems.userItems} setChosenItems={setChosenItems} items={"userItems"} />
+          </div>
+
+          <div className="flex flex-col gap-3 w-full h-1/2">
+            <p className="font-logo text-sm border-b border-b-orange-800 ">Your Offer</p>
+            <ChosenItemsSection chosenItems={chosenItems.myItems} setChosenItems={setChosenItems} items={"myItems"} />
+          </div>
         </div>
       </div>
 
-      {/* OFFER SECTION */}
-
-      <div className="flex flex-col gap-3 w-full h-[500px]">
-        <div className="flex flex-col gap-3 w-full h-1/2">
-          <p className="font-logo text-sm border-b border-b-orange-800 w-full">You Want</p>
-          <ChosenItemsSection chosenItems={chosenItems.userItems} setChosenItems={setChosenItems} items={"userItems"} />
-        </div>
-
-        <div className="flex flex-col gap-3 w-full h-1/2">
-          <p className="font-logo text-sm border-b border-b-orange-800 ">Your Offer</p>
-          <ChosenItemsSection chosenItems={chosenItems.myItems} setChosenItems={setChosenItems} items={"myItems"} />
-        </div>
+      <div className="flex flex-row w-full justify-center items-center">
+        <button className="w-36 h-12 duration-100 bg-orange-800 rounded-md font-logo text-white hover:bg-orange-600">Make the Offer</button>
       </div>
 
     </div>
