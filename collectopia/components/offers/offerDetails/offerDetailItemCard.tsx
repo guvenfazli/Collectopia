78468c85e644ec8b06420572faa3e0eb77e90021
Güven Fazlı <guven.fazli@gmail.com>
2025-01-10 05:53:09 +0300

    Rendering the first prototype of offer detail is fixed

--- collectopia/components/offers/offerDetails/offerDetailItemCard.tsx
+++ collectopia/components/offers/offerDetails/offerDetailItemCard.tsx
@@ -1,8 +1,29 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel"
+import Image from "next/image"
+
 export default function OfferDetailItemCard({ offeredItem }: any) {
 
   return (
-    <div className="flex w-1/4 items-start justify-start">
-      <p>{offeredItem.title}</p>
+    <div className="flex flex-col w-1/3 border border-orange-800 p-1 items-start justify-start">
+      <Carousel className="w-full mb-2">
+        <CarouselContent className="h-96">
+          {offeredItem.imageList.map((img: string) =>
+            <CarouselItem className="relative" key={img}>
+              <div className="absolute blur-sm top-0 right-0 bottom-0 left-0" style={{ background: `url(http://localhost:8080/${img.replaceAll(/\\/g, "/")})`, backgroundPosition: "center", backgroundSize: "contain" }} />
+              <Image
+                fill
+                alt="uploadedImage"
+                src={`http://localhost:8080/${img.replaceAll(/\\/g, "/")}`}
+                style={{ objectFit: "contain" }} />
+            </CarouselItem>
+          )}
+        </CarouselContent>
+      </Carousel>
+      <p className="text-lg tracking-wide ">{offeredItem.title}</p>
     </div>
   )
 }
--- collectopia/components/offers/offerDetails/receivedOfferDetail.tsx
+++ collectopia/components/offers/offerDetails/receivedOfferDetail.tsx
@@ -12,9 +12,13 @@ export default function ReceivedOfferDetail({ chosenOffer }: ComponentProps) {
   console.log(chosenOffer)
 
   return (
-    <div className="flex flex-col w-full justify-start items-start gap-3 p-1">
-      <div className="flex flex-row w-full justify-start items-start gap-2 p-1">
+    <div className="flex flex-col w-full justify-start items-start gap-3 bg-orange-200 p-1">
+      <div className="flex flex-row w-full justify-start items-start gap-2 p-1 overflow-scroll overflow-y-hidden" style={{ scrollbarWidth: 'thin', scrollbarColor: "#9A3412 transparent", WebkitOverflowScrolling: "touch" }}>
         {chosenOffer.offer.offeredItems.map((offeredItem: any) => <OfferDetailItemCard key={offeredItem._id} offeredItem={offeredItem} />)}
+      </div>
+
+      <div className="flex flex-row w-full justify-start items-start gap-2 p-1 overflow-scroll overflow-y-hidden" style={{ scrollbarWidth: 'thin', scrollbarColor: "#9A3412 transparent", WebkitOverflowScrolling: "touch" }}>
+        {chosenOffer.offer.wantedItems.map((wantedItem: any) => <OfferDetailItemCard key={wantedItem._id} offeredItem={wantedItem} />)}
       </div>
 
     </div>
